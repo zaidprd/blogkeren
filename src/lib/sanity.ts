4b62284aa66e5e759e4c32f2fb76f7f076e9a478
zaidprd <160503080+zaidprd@users.noreply.github.com>
2025-09-22 12:08:36 +0700

    feat: Use environment variables for Sanity

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,10 +1,8 @@
-// src/lib/sanity.js
-
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-  projectId: 'nfclqqvk', 
-  dataset: 'production',
-  apiVersion: '2023-05-03', // Gunakan tanggal hari ini atau versi lain
-  useCdn: true, // Gunakan CDN untuk performa lebih baik di production
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
+  apiVersion: '2023-05-03',
+  useCdn: true,
 });
